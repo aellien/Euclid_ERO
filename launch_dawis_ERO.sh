6511488f535e113c43b7fa70c8d47094ab0bc660
aellien <ellien@iap.fr>
2024-02-09 16:45:50 +0100

    bug

--- launch_dawis_ERO.sh
+++ launch_dawis_ERO.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#
-#
 
 for file in Euclid-NISP-H-ERO-Abell2390-LSB.DR3.crop.fits Euclid-NISP-J-ERO-Abell2390-LSB.DR3.crop.fits Euclid-NISP-Y-ERO-Abell2390-LSB.DR3.crop.fits
+do
       echo "Launch Dawis on file $file"
       qsub qsub_dawis_ERO.sh -v n=${file},ncl=A2390 #${file:0:-5}
 done
