c614ae38c5f2b5d08964e354361357b9080307cd
aellien <ellien@iap.fr>
2024-02-12 10:28:15 +0100

    utils

--- qsub_dawis_ERO.sh
+++ qsub_dawis_ERO.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -o /home/ellien/Euclid_ERO/logs/icl_ERO_${ncl}.out
+#PBS -o /home/ellien/Euclid_ERO/logs/icl_ERO_${ncl}_${band}.out
 #PBS -j oe
 #PBS -N icl_JWST
 #PBS -l nodes=1:ppn=8,walltime=47:59:00
@@ -7,6 +7,7 @@
 
 module load intelpython/3-2020.4
 echo ${n}
+echo ${band}
 python /home/ellien/Euclid_ERO/Euclid_ERO_scripts/dawis_ERO.py ${n}
 
 exit 0
