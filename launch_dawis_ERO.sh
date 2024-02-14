12de5c5d28a08f635d47658076154ddada8033b5
aellien <ellien@iap.fr>
2024-02-14 11:10:08 +0100

    small display bug fix

--- launch_dawis_ERO.sh
+++ launch_dawis_ERO.sh
@@ -3,6 +3,6 @@
 for file in Euclid-NISP-H-ERO-Abell2390-LSB.DR3.crop.fits Euclid-NISP-J-ERO-Abell2390-LSB.DR3.crop.fits Euclid-NISP-Y-ERO-Abell2390-LSB.DR3.crop.fits
 do
       echo "Launch Dawis on file $file"
-      band=${a:12:1}
-      qsub qsub_dawis_ERO.sh -v n=${file},ncl=A2390,band=${a:12:1} #${file:0:-5}
+      band=${file:12:1}
+      qsub qsub_dawis_ERO.sh -v n=${file},ncl=A2390,band=${file:12:1} #${file:0:-5}
 done
