c0f9d31edd1513ddede7004851753f2ee54ac2ee
aellien <ellien@iap.fr>
2024-02-15 16:05:27 +0100

    out3, no Haar

--- dawis_ERO.py
+++ dawis_ERO.py
@@ -1,6 +1,6 @@
 import os
 import sys
-#sys.path.append("/home/aellien/dawis")
+sys.path.append("/home/aellien/dawis")
 import dawis
 import shutil
 import cProfile
@@ -10,20 +10,20 @@ indir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3'
 #indir = '/home/aellien/Euclid_ERO/data/Euclid-NISP-Stack-ERO-Abell2390.DR3'
 infile = sys.argv[1]
 #infile = "Euclid-NISP-H-ERO-Abell2390-LSB.DR3.crop.fits"
-outdir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3/wavelets/out2/'
-#outdir = '/home/aellien/Euclid_ERO/wavelets/local/'
+outdir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3/wavelets/out3/'
+#outdir = '/home/aellien/Euclid_ERO/wavelets/local/run5'
 
 n_cpus = 8 # Number of CPUs
-tau = 0.05   # Relative Threshold
-gamma = 0.2   # Attenuation (CLEAN) factor
+tau = 0.9   # Relative Threshold
+gamma = 0.1   # Attenuation (CLEAN) factor
 ceps = 5E-6    # Convergence value for epsilon
 n_levels = 11    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 2    # Maximum number of wavelet scales spanned by an interscale tree
 lvl_sep_big = 5     # Scale at wich mix_span, max_span & gamma are set to 1, and monomodality is enforced
-extent_sep = 0.15   # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
-ecc_sep = 0.9      # Eccentricity threshold over which the Haar wavelet is used for restoration
-lvl_sep_lin = 3     # Wavelet scale under which the Haar wavelet can be used for restoration
+extent_sep = 0.   # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
+ecc_sep = 1.0      # Eccentricity threshold over which the Haar wavelet is used for restoration
+lvl_sep_lin = -1     # Wavelet scale under which the Haar wavelet can be used for restoration
 max_iter = 1500      # Maximum number of iterations
 data_dump = True    # Write data at each iteration /!\ demands lot of space on hardware /!\
 gif = True      # Make gifs of the run (need data_dump = True)
