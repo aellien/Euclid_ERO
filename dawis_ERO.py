a3963a7adbb74c2527d8e2988602e4e156e110b4
aellien <ellien@iap.fr>
2024-02-24 12:14:11 +0100

    out8

--- dawis_ERO.py
+++ dawis_ERO.py
@@ -10,20 +10,20 @@ indir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3'
 #indir = '/home/aellien/Euclid_ERO/data/Euclid-NISP-Stack-ERO-Abell2390.DR3'
 infile = sys.argv[1]
 #infile = "Euclid-NISP-J-ERO-Abell2390-LSB.DR3.crop.fits"
-outdir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3/wavelets/out7/'
+outdir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3/wavelets/out8/'
 #outdir = '/home/aellien/Euclid_ERO/wavelets/local/run10'
 
 n_cpus = 8 # Number of CPUs
 tau = 0.8   # Relative Threshold
-gamma = 0.5   # Attenuation (CLEAN) factor
+gamma = 0.2   # Attenuation (CLEAN) factor
 ceps = 1E-4    # Convergence value for epsilon
 n_levels = 9    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 2    # Maximum number of wavelet scales spanned by an interscale tree
-lvl_sep_big = 5     # Scale at wich mix_span, max_span & gamma are set to 1, and monomodality is enforced
+lvl_sep_big = 8     # Scale at wich mix_span, max_span & gamma are set to 1, and monomodality is enforced
 extent_sep = 0.1   # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
 ecc_sep = 0.05      # Eccentricity threshold over which the Haar wavelet is used for restoration
-lvl_sep_lin = 2     # Wavelet scale under which the Haar wavelet can be used for restoration
+lvl_sep_lin = -1     # Wavelet scale under which the Haar wavelet can be used for restoration
 max_iter = 1500      # Maximum number of iterations
 data_dump = True    # Write data at each iteration /!\ demands lot of space on hardware /!\
 gif = True      # Make gifs of the run (need data_dump = True)
