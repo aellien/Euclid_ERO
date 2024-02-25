12af67958deb82a8120a163823de9748669f1e45
aellien <ellien@iap.fr>
2024-02-25 20:38:08 +0100

    out9

--- dawis_ERO.py
+++ dawis_ERO.py
@@ -10,17 +10,17 @@ indir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3'
 #indir = '/home/aellien/Euclid_ERO/data/Euclid-NISP-Stack-ERO-Abell2390.DR3'
 infile = sys.argv[1]
 #infile = "Euclid-NISP-J-ERO-Abell2390-LSB.DR3.crop.fits"
-outdir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3/wavelets/out8/'
+outdir = '/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3/wavelets/out9/'
 #outdir = '/home/aellien/Euclid_ERO/wavelets/local/run10'
 
-n_cpus = 8 # Number of CPUs
-tau = 0.8   # Relative Threshold
-gamma = 0.2   # Attenuation (CLEAN) factor
+n_cpus = 2 # Number of CPUs
+tau = 0.95   # Relative Threshold
+gamma = 0.1   # Attenuation (CLEAN) factor
 ceps = 1E-4    # Convergence value for epsilon
-n_levels = 9    # Number of wavelet scales
+n_levels = 10    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 2    # Maximum number of wavelet scales spanned by an interscale tree
-lvl_sep_big = 8     # Scale at wich mix_span, max_span & gamma are set to 1, and monomodality is enforced
+lvl_sep_big = 5     # Scale at wich mix_span, max_span & gamma are set to 1, and monomodality is enforced
 extent_sep = 0.1   # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
 ecc_sep = 0.05      # Eccentricity threshold over which the Haar wavelet is used for restoration
 lvl_sep_lin = -1     # Wavelet scale under which the Haar wavelet can be used for restoration
@@ -34,13 +34,14 @@ threshold_rel = 0.15
 size_patch = 100
 resume = True
 rm_gamma_for_big = True
+iptd_sigma = 1
 
 shutil.copyfile( os.path.abspath(__file__), os.path.join( outdir, infile[:-4] + 'input.dawis.py' ) )
 
 dawis.synthesis_by_analysis( indir = indir, infile = infile, outdir = outdir, n_cpus = n_cpus, n_levels = n_levels, \
                                     tau = tau, gamma = gamma, ceps = ceps, conditions = conditions, min_span = min_span, \
                                     max_span = max_span, lvl_sep_big = lvl_sep_big, monomodality = monomodality, threshold_rel = threshold_rel, \
-                                    max_iter = max_iter, extent_sep = extent_sep, ecc_sep = ecc_sep, size_patch = size_patch, rm_gamma_for_big = rm_gamma_for_big, \
+                                    max_iter = max_iter, extent_sep = extent_sep, ecc_sep = ecc_sep, iptd_sigma = iptd_sigma, size_patch = size_patch, rm_gamma_for_big = rm_gamma_for_big, \
                                     data_dump = data_dump, gif = gif, resume = resume )
 
 '''start = datetime.now()
---
+++ rm_and_ipt_stars.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 13:44:37 2024
+
+@author: aellien
+"""
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Modules
+import sys
+sys.path.append("/home/aellien/dawis")
+import glob as glob
+import os
+import numpy as np
+import pyregion as pyr
+import random
+import pandas as pd
+import dawis as d
+import pandas as pd
+import photutils as phut
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.visualization import *
+from astropy.convolution import convolve
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if __name__ == '__main__':
+
+    # Paths, lists & variables
+    path_data = '/home/aellien/Euclid_ERO/data/Euclid-NISP-Stack-ERO-Abell2390.DR3'
+    path_scripts = '/home/aellien/Euclid_ERO/Euclid_ERO_scripts'
+    path_wavelets = '/home/aellien/Euclid_ERO/wavelets/out5/'
+    path_plots = '/home/aellien/Euclid_ERO/plots'
+    path_analysis = '/home/aellien/Euclid_ERO/analysis/'
+    
+    hdu = fits.open(os.path.join(path_data, 'Euclid-NISP-H-ERO-Abell2390-LSB.DR3.crop.fits'))
+    im = hdu[0].data
+    head = hdu[0].header
+    w = WCS(head)
+    
+    data = np.copy(im)
+    bkg_estimator = phut.background.MedianBackground()
+    bkg = phut.background.Background2D(data, (50, 50), filter_size=(3, 3), bkg_estimator=bkg_estimator)
+    data -= bkg.background
+    kernel = phut.segmentation.make_2dgaussian_kernel(5.0, size=25)  # FWHM = 3.0
+    convolved_data = convolve(data, kernel)
+    
+    '''norm = ImageNormalize(data, interval = ZScaleInterval())
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12.5))
+    ax1.imshow(data, origin='lower', cmap='Greys_r', norm=norm)
+    ax1.set_title('Background-subtracted Data')
+    ax2.imshow(segment_map, origin='lower', cmap=segment_map.cmap, interpolation='nearest')
+    ax2.set_title('Segmentation Image')'''
+
+    threshold = 2. * bkg.background_rms
+    finder = phut.segmentation.SourceFinder(npixels = 10, progress_bar = True )
+    #segment_map = finder(convolved_data, threshold)
+    segment_map = phut.segmentation.detect_sources(convolved_data, threshold, npixels=10)
+    
+    bboxl = [ x.extent for x in segment_map.bbox ]
+    centerl = [ x.center for x in segment_map.bbox ]
+    extentl = []
+    for i, (bbox, center, area) in enumerate( zip(bboxl, centerl, segment_map.areas)):
+        tot_area = (bbox[1] - bbox[0]) * (bbox[3] - bbox[2])
+        extent = area / tot_area
+        extentl.append(extent)
+    extentl = np.array(extentl)
+    
+    sorted_labels = segment_map.labels[ np.argsort( segment_map.areas )][-20:]
+    sorted_extentl = extentl[ np.argsort( segment_map.areas )][-20:]
+    
+    mask = np.zeros(data.shape)
+    for i in sorted_labels:
+        if segment_map.data[1800, 1800] != i:
+            mask[np.where(segment_map.data == i)] = 1
+
+    ''' norm = ImageNormalize(data, interval = AsymmetricPercentileInterval(40, 99.5), stretch = LogStretch())
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 12.5))
+    ax1.imshow(data, origin='lower', cmap='Greys_r', norm=norm)
+    ax1.set_title('Background-subtracted Data')
+    ax2.imshow(mask, origin='lower', cmap=segment_map.cmap,
+               interpolation='nearest')
+    ax2.set_title('Segmentation Image')
+    plt.savefig('/home/aellien/Euclid_ERO/plots/seg.png', format = 'png', dpi = 1000)
+    '''
+    iptd_im = np.copy(im)
+    noise = d.sample_noise(iptd_im, n_sigmas = 3)
+    draws = np.random.normal(noise.mean(), noise.std(), iptd_im.shape)
+    mask *= draws
+    iptd_im[ mask > 1 ] = 0.
+    iptd_im += mask
+    
+    norm = ImageNormalize(iptd_im, interval = AsymmetricPercentileInterval(40, 99.5), stretch = LogStretch())
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 12.5))
+    ax1.imshow(iptd_im, origin='lower', cmap='Greys_r', norm=norm)
+    ax1.set_title('Background-subtracted Data')
+    ax2.imshow(mask, origin='lower', cmap=segment_map.cmap,
+               interpolation='nearest')
+    ax2.set_title('Segmentation Image')
+    plt.savefig('/home/aellien/Euclid_ERO/plots/seg.png', format = 'png', dpi = 1000)
