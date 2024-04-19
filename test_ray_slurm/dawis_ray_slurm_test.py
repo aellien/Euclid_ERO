b39ec140c20e71a00c3dc2048ecdd950cc424721
aellien <ellien@iap.fr>
2024-04-19 10:52:08 +0200

    190420241052

--- test_ray_slurm/dawis_ray_slurm_test.py
+++ test_ray_slurm/dawis_ray_slurm_test.py
@@ -5,13 +5,14 @@ Created on Fri Apr 19 10:40:26 2024
 
 @author: aellien
 """
+import ray
 @ray.remote
 def f(x):
     return x * x
 
 if __name__ == "__main__":
     
-    import ray
+    
     ray.init()
     futures = [f.remote(i) for i in range(1000)]
     print(ray.get(futures)) # [0, 1, 4, 9]
