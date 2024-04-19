b4f9d3ab8d33e344d886da52b5f5e7bc836de226
aellien <ellien@iap.fr>
2024-04-19 14:15:25 +0200

    interactive head

--- test_ray_slurm/dawis_ray_slurm_test.py
+++ test_ray_slurm/dawis_ray_slurm_test.py
@@ -1,22 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 19 10:40:26 2024
-
-@author: aellien
-"""
 import ray
 @ray.remote
 def f(x):
+    print('coucou2')
+    print(ray.runtime_context.RuntimeContext.get_node_id())
     return x * x
 
 if __name__ == "__main__":
-    
-    
+
+
     ray.init()
-    futures = [f.remote(i) for i in range(1000)]
+    print('coucouc1')
+    futures = [f.remote(i) for i in range(4)]
     print(ray.get(futures)) # [0, 1, 4, 9]
     ray.close()
-    
-    
-    
