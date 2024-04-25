f01f3b385505646fc4fd0be5d6db503305d15ec0
aellien <ellien@iap.fr>
2024-04-25 11:06:27 +0200

    250420241106

--- slurm_dawis_ray_test.sh
+++ slurm_dawis_ray_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=euclid_ERO
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=4
 #SBATCH --time=48:00:00
 #SBATCH --output /n03data/ellien/Euclid_ERO/logs/%x.%j.out 
 #SBATCH --error  /n03data/ellien/Euclid_ERO/logs/%x.%j.err
@@ -10,5 +10,5 @@
 source /home/ellien/.bashrc
 conda activate dawis
 
-python -W"ignore" /home/ellien/Euclid_ERO/Euclid_ERO_scripts/dawis_ERO_ray_test.py
+python -u -W"ignore" /home/ellien/Euclid_ERO/Euclid_ERO_scripts/dawis_ERO_ray_test.py
 exit 0
