9c62ece12253eccaa50dbe9628d083876f13fbfc
aellien <ellien@iap.fr>
2024-06-24 11:27:42 +0200

    240620241127

--- slurm_make_results_ERO_noray.sh
+++ slurm_make_results_ERO_noray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=synth_Euclid_ERO
-#SBATCH --nodelist=n01
+#SBATCH --nodelist=n09
 #SBATCH --ntasks-per-node=3
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
@@ -12,7 +12,8 @@ conda activate dawis
 
 path_data='/n03data/ellien/Euclid_ERO/Euclid-NISP-Stack-ERO-Abell2390.DR3'
 cd $path_data
-for nf in "/Euclid-NISP-?-ERO-Abell2390-LSB.DR3.crop.fits"
+echo $(pwd)
+for nf in "Euclid-NISP-?-ERO-Abell2390-LSB.DR3.crop.fits"
 do
 echo "launch $nf."
 python /home/ellien/Euclid_ERO/Euclid_ERO_scripts/make_results_ERO.py $nf 5 80 1000 6 10 9 10 &
