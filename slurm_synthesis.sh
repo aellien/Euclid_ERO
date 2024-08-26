e1a31800b674154d8218785833adc9d801fc4311
aellien <ellien@iap.fr>
2024-08-26 09:43:31 +0200

    out14

--- slurm_synthesis.sh
+++ slurm_synthesis.sh
@@ -4,12 +4,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
-#SBATCH --output /n03data/ellien/Euclid_ICL/logs/synthesis.%x.%j.out 
-#SBATCH --error  /n03data/ellien/Euclid_ICL/logs/synthesis.%x.%j.err
+#SBATCH --output /n03data/ellien/Euclid_ERO/logs/synthesis.%x.%j.out 
+#SBATCH --error  /n03data/ellien/Euclid_ERO/logs/synthesis.%x.%j.err
 
 source /home/ellien/.bashrc
 conda activate dawis
 
-python /home/ellien/Euclid_ICL/Euclid_scripts/synthesis_simulations.py
+python /home/ellien/Euclid_ERO/Euclid_ERO_scripts/synthesis_simulations.py
 
 exit 0
